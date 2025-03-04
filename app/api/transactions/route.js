528ed5c655e3f3e08b336094732bc9c1ce121ce3
Henil Tandel <heniltandel0905@gmail.com>
2025-03-04 14:25:42 +0530

    updated

--- app/api/transactions/route.js
+++ app/api/transactions/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { MongoClient, ObjectId } from "mongodb";
 
-const uri = "mongodb+srv://heniltandel0905:heniltandel@cluster0.qqhwf.mongodb.net/";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 export async function GET() {
